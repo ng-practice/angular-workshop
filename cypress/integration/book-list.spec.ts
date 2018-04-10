32abe01bd41d4dfd9724e1a14997099475762da2
Gregor Woiwode <gregor.woiwode@sap.com>
2018-04-10 13:54:11 +0200

    cypress: add test interacting with elements

--- cypress/integration/book-list.spec.ts
+++ cypress/integration/book-list.spec.ts
@@ -1,10 +1,18 @@
 describe('Visiting the start page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4200');
+    cy.get('.title').as('title');
   });
 
   it('should display the header "Books"', () => {
-    cy.get('.title').contains(/books/i);
+    cy.get('@title').contains(/books/i);
+  });
+
+  it('should only have one heading', () => {
+    cy
+      .get('@title')
+      .then(found => found.length)
+      .then(count => expect(count).to.eq(1));
   });
 
   it('should redirect to books list', () => {
---
+++ cypress/integration/create-book.spec.ts
@@ -0,0 +1,50 @@
+describe('Creating a new book', () => {
+  const isbn = '093-213123-1231-2';
+
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+    cy.get('.mat-card').as('books');
+  });
+
+  it('should increase the count of the book items by one', () => {
+    let booksCountBefore = 0;
+
+    cy
+      .get('@books')
+      .then(books => (booksCountBefore = books.length))
+      .then(_ => click('.mat-raised-button', /create/i))
+      .then(_ => fillForm(isbn))
+      .then(_ => click('.mat-raised-button', /save/i))
+      .then(_ => cy.visit('http://localhost:4200'))
+      .then(_ => cy.get('@books'))
+      .then(books => expect(books.length).eq(booksCountBefore + 1));
+  });
+
+  afterEach(() =>
+    cy
+      .get('@books')
+      .last()
+      .find('button')
+      .click()
+  );
+});
+
+function fillForm(isbn: string) {
+  return cy
+    .get(formControl('isbn'))
+    .type(isbn)
+    .then(() => cy.get(formControl('title')).type('Angular 6-rc.3'))
+    .then(() => cy.get(formControl('author')).type('Misko Hevery'))
+    .then(() => cy.get(formControl('abstract')).type('useful information.'));
+
+  function formControl(name: string) {
+    return `[formcontrolname="${name}"]`;
+  }
+}
+
+function click(selector: string, textContent: RegExp) {
+  return cy
+  .get(selector)
+  .contains(textContent)
+  .click();
+}
