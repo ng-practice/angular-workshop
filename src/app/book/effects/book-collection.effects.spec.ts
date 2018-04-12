f1a0e22f0dcf7dd627aa6be2b67591fb0195ce9d
Gregor Woiwode <gregor.woiwode@sap.com>
2018-04-12 13:25:15 +0200

    test: add negative test for effect

--- src/app/book/effects/book-collection.effects.spec.ts
+++ src/app/book/effects/book-collection.effects.spec.ts
@@ -1,6 +1,8 @@
 import { Actions } from '@ngrx/effects';
 import { ReplaySubject, Observable, of } from 'rxjs';
 
+import { _throw } from 'rxjs/observable/throw';
+
 import {
   Load,
   BookCollectionActionTypes
@@ -9,11 +11,11 @@ import { BookDataService } from '../shared/book-data.service';
 import { BookCollectionEffects } from './book-collection.effects';
 
 describe('effect: book-collection', () => {
+  let loadAction$: ReplaySubject<Load>;
+  let actions$: Actions;
+  let service: BookDataService;
+  let effects: BookCollectionEffects;
   describe('When the books are loaded successfully', () => {
-    let loadAction$: ReplaySubject<Load>;
-    let actions$: Actions;
-    let service: BookDataService;
-    let effects: BookCollectionEffects;
 
     beforeEach(() => {
       loadAction$ = new ReplaySubject(1);
@@ -31,6 +33,28 @@ describe('effect: book-collection', () => {
     it('should yield a success action', done => {
       effects.load.subscribe(action => {
         expect(action.type).toBe(BookCollectionActionTypes.LoadSuccess);
+        done();
+      });
+    });
+  });
+
+  describe('When an error is while loading the books', () => {
+    beforeEach(() => {
+      loadAction$ = new ReplaySubject(1);
+      loadAction$.next(new Load());
+
+      // First dep for effect
+      actions$ = new Actions(loadAction$);
+
+      // Second dep for effect
+      service = stubBookDataService(_throw(new Error()));
+
+      effects = new BookCollectionEffects(actions$, service);
+    });
+
+    it('should yield a LoadError Action', done => {
+      effects.load.subscribe(action => {
+        expect(action.type).toBe(BookCollectionActionTypes.LoadError);
         done();
       });
     });
