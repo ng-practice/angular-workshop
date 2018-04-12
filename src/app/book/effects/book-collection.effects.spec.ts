d7b3a74b309c47b5c1ec2df9a9a377a6f5b0b1e0
Gregor Woiwode <gregor.woiwode@sap.com>
2018-04-12 07:55:32 +0200

    effect testing: test effect with a ReplaySubject

--- src/app/book/effects/book-collection.effects.spec.ts
+++ src/app/book/effects/book-collection.effects.spec.ts
@@ -1,20 +1,26 @@
 import { Actions } from '@ngrx/effects';
-import { hot } from 'jasmine-marbles';
-import { Observable, of } from 'rxjs';
+import { Observable, of, ReplaySubject } from 'rxjs';
 
-import { Load, LoadSuccess } from '../actions/book-collection.actions';
+import {
+  Load,
+  LoadSuccess,
+  BookCollectionActionTypes
+} from '../actions/book-collection.actions';
 import { BookCollectionEffects } from './book-collection.effects';
 
 describe('(effect) book-collection', () => {
   describe('When a draft is saved successfully', () => {
-    const actions$ = new Actions(hot('-a-', { a: new Load() }));
+    const loadAction = new ReplaySubject<Load>(1);
+    loadAction.next(new Load());
+    const actions$ = new Actions(loadAction);
     const service = stubBookDataService(of([]));
     const effects = new BookCollectionEffects(actions$, service);
 
-    it('should yield a success notification', () => {
-      expect(effects.load).toBeObservable(
-        hot('-a-', { a: new LoadSuccess([]) })
-      );
+    it('should yield a success notification', done => {
+      effects.load.subscribe(action => {
+        expect(action.type).toBe(BookCollectionActionTypes.LoadSuccess);
+        done();
+      });
     });
   });
 });
